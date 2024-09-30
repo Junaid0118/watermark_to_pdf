0392118b342326a860b0d87281f4ce65e3aec02c
junaid-dev <junaidiqbalshah011@gmail.com>
2024-09-30 19:48:50 +0500

    Initialize gem repo

---
+++ lib/watermark_to_pdf.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "watermark_to_pdf/version"
+
+module WatermarkToPdf
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/watermark_to_pdf/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WatermarkToPdf
+  VERSION = "0.1.0"
+end
